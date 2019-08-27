961e664b2cccb34b71fe73374d20a26a61f0b7c6
jaouadhba <jaouadhba@gmail.com>
2019-08-28 01:31:59 +0200

    Modifier l'emplacement de la classe Calculator.java

--- src/test/java/fr/reference/it/CalculatorTest.java
+++ src/test/java/fr/reference/it/CalculatorTest.java
@@ -3,7 +3,6 @@ package fr.reference.it;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert.*;
 
 import java.math.BigDecimal;
 
@@ -26,23 +25,5 @@ public class CalculatorTest {
         Assert.assertEquals(BigDecimal.valueOf(12),calculator.getAccumulator());
     }
 
-    @Test
-    public void testEnterAndDropAccumulator()
-    {
-        BigDecimal val1 = BigDecimal.valueOf(12);
-        BigDecimal val2 = BigDecimal.valueOf(20);
-        BigDecimal val3 = BigDecimal.valueOf(30);
 
-        calculator.setAccumulator(val1);
-        calculator.enter();
-        calculator.setAccumulator(val2);
-        calculator.enter();
-        calculator.setAccumulator(val3);
-        Assert.assertEquals(val3,calculator.getAccumulator());
-        calculator.drop();
-        Assert.assertEquals(val2,calculator.getAccumulator());
-        calculator.drop();
-        Assert.assertEquals(val1,calculator.getAccumulator());
-
-    }
 }
